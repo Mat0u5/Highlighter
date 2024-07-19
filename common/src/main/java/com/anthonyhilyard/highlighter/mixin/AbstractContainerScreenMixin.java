d4ee3305bac0c3779b6bafa4bd16e48d96c32712
Anthony Hilyard <anthony.hilyard@gmail.com>
2024-07-19 09:56:30 -0700

    Fixed NeoForge mixins not loading.

--- common/src/main/java/com/anthonyhilyard/highlighter/mixin/AbstractContainerScreenMixin.java
+++ common/src/main/java/com/anthonyhilyard/highlighter/mixin/AbstractContainerScreenMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.Group;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.gui.GuiGraphics;
@@ -13,6 +14,7 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.network.chat.Component;
 
 @Mixin(AbstractContainerScreen.class)
@@ -20,9 +22,24 @@ public class AbstractContainerScreenMixin extends Screen
 {
 	protected AbstractContainerScreenMixin(Component titleIn) { super(titleIn); }
 
+	@Group(name = "renderSlot")
 	@Inject(method = "renderSlot", at = @At(value = "INVOKE",
 			target = "Lnet/minecraft/client/gui/GuiGraphics;renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V", shift = Shift.AFTER))
 	public void renderSlot(GuiGraphics graphics, Slot slot, CallbackInfo info)
+	{
+		// Only mark items that are in the player's inventory.
+		if (slot.container instanceof Inventory)
+		{
+			if (slot.hasItem() && ((Inventory)slot.container).items.contains(slot.getItem()))
+			{
+				Highlighter.renderNewItemMark(graphics.pose(), slot);
+			}
+		}
+	}
+
+	@Group(name = "renderSlot")
+	@Inject(method = "renderSlotContents", at = @At(value = "TAIL"), remap = false)
+	public void renderSlotContents(GuiGraphics graphics, ItemStack itemStack, Slot slot, String countString, CallbackInfo info)
 	{
 		// Only mark items that are in the player's inventory.
 		if (slot.container instanceof Inventory)
