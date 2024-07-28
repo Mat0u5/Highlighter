87ac17a281ccdcb93cf8fa68e8510dba352a8277
Anthony Hilyard <anthony.hilyard@gmail.com>
2024-07-28 15:02:37 -0700

    Fixed Forge build.

--- fabric/src/main/java/com/anthonyhilyard/highlighter/fabric/mixin/AbstractContainerScreenMixin.java
+++ fabric/src/main/java/com/anthonyhilyard/highlighter/fabric/mixin/AbstractContainerScreenMixin.java
@@ -1,4 +1,4 @@
-package com.anthonyhilyard.highlighter.mixin;
+package com.anthonyhilyard.highlighter.fabric.mixin;
 
 import com.anthonyhilyard.highlighter.Highlighter;
 
@@ -6,7 +6,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At.Shift;
-import org.spongepowered.asm.mixin.injection.Group;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.gui.GuiGraphics;
@@ -14,7 +13,6 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.Slot;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.network.chat.Component;
 
 @Mixin(AbstractContainerScreen.class)
@@ -22,24 +20,9 @@ public class AbstractContainerScreenMixin extends Screen
 {
 	protected AbstractContainerScreenMixin(Component titleIn) { super(titleIn); }
 
-	@Group(name = "renderSlot")
 	@Inject(method = "renderSlot", at = @At(value = "INVOKE",
 			target = "Lnet/minecraft/client/gui/GuiGraphics;renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V", shift = Shift.AFTER))
 	public void renderSlot(GuiGraphics graphics, Slot slot, CallbackInfo info)
-	{
-		// Only mark items that are in the player's inventory.
-		if (slot.container instanceof Inventory)
-		{
-			if (slot.hasItem() && ((Inventory)slot.container).items.contains(slot.getItem()))
-			{
-				Highlighter.renderNewItemMark(graphics.pose(), slot);
-			}
-		}
-	}
-
-	@Group(name = "renderSlot")
-	@Inject(method = "renderSlotContents", at = @At(value = "TAIL"), remap = false)
-	public void renderSlotContents(GuiGraphics graphics, ItemStack itemStack, Slot slot, String countString, CallbackInfo info)
 	{
 		// Only mark items that are in the player's inventory.
 		if (slot.container instanceof Inventory)
---
+++ neoforge/src/main/java/com/anthonyhilyard/highlighter/neoforge/mixin/AbstractContainerScreenMixin.java
@@ -0,0 +1,35 @@
+package com.anthonyhilyard.highlighter.neoforge.mixin;
+
+import com.anthonyhilyard.highlighter.Highlighter;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.network.chat.Component;
+
+@Mixin(AbstractContainerScreen.class)
+public class AbstractContainerScreenMixin extends Screen
+{
+	protected AbstractContainerScreenMixin(Component titleIn) { super(titleIn); }
+
+	@Inject(method = "renderSlotContents", at = @At(value = "TAIL"), remap = false)
+	public void renderSlotContents(GuiGraphics graphics, ItemStack itemStack, Slot slot, String countString, CallbackInfo info)
+	{
+		// Only mark items that are in the player's inventory.
+		if (slot.container instanceof Inventory)
+		{
+			if (slot.hasItem() && ((Inventory)slot.container).items.contains(slot.getItem()))
+			{
+				Highlighter.renderNewItemMark(graphics.pose(), slot);
+			}
+		}
+	}
+}
