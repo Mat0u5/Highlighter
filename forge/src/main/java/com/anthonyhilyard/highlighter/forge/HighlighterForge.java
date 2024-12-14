3d0a638fbc2b87097f92849d8e22b6258b8c9f8f
Anthony Hilyard <anthony.hilyard@gmail.com>
2024-12-14 12:39:20 -0800

    Updated dependency definitions.
    Fixed Forge loading issue.

--- forge/src/main/java/com/anthonyhilyard/highlighter/forge/HighlighterForge.java
+++ forge/src/main/java/com/anthonyhilyard/highlighter/forge/HighlighterForge.java
@@ -3,13 +3,12 @@ package com.anthonyhilyard.highlighter.forge;
 import com.anthonyhilyard.highlighter.Highlighter;
 
 import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(Highlighter.MODID)
 public final class HighlighterForge
 {
-	public HighlighterForge(ModLoadingContext context)
+	public HighlighterForge(FMLJavaModLoadingContext context)
 	{
 		context.registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "ANY", (remote, isServer) -> true));
 	}
